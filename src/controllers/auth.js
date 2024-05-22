ef6d0f170aa9fbc94aea14feb6769b6c6f62ff5a
Minky1 <151859285+Minky1@users.noreply.github.com>
2024-05-22 11:46:34 +0300

    Delete src directory

--- src/controllers/auth.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const login = async (req, res) => {
-  const response = await axios
-    .get("http://localhost:3000/users")
-    .then((res) => res.data);
-  return res.status(200).json(response.data);
-};
